18a96c81f651b47b1c4a499c4f62fbd3cfd19d5a
DESKTOP-N967G0J\rober <roberto.taveras@hotmail.com>
2021-03-13 21:23:22 -0400

    no message

--- BusinessObjects/Helpers/HelperAssignProperty.cs
+++ BusinessObjects/Helpers/HelperAssignProperty.cs
@@ -13,11 +13,10 @@ namespace BusinessObjects.Helpers
             {
                 var result = propertiestFromSet.Where(k => k.Name == p.Name).FirstOrDefault();
                 if (result == null)
-                    throw new InvalidCastException($"La propiedad {p.Name} toSet tiene un tipo de dato { p.GetType()} y fromSet tiene un tipo de dato {result.GetType()}  {instanceFrom} !!");
+                    throw new InvalidCastException($"La propiedad {p.Name} de {instanceToSet} no existe en {instanceFrom} ..!!");
 
                 if(p.GetType() !=  result.GetType())
-                    throw new InvalidCastException($"La propiedad {p.Name} y  la instancia {instanceFrom} tienen tipos de datos diferentes !!");
-
+                    throw new InvalidCastException($"La propiedad {p.Name} toSet tiene un tipo de dato { p.GetType()} y fromSet tiene un tipo de dato {result.GetType()}  {instanceFrom} ..!!");
                 try
                 {
                     p.SetValue(toSet, result.GetValue(fromSet,null), null);
