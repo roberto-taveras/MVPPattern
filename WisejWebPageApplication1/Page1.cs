1210d0221a9c1912e69959d6b1a0980cc6c8785f
= <=>
2021-04-09 20:15:30 -0400

    Se separaron los presenters

---
+++ WisejWebPageApplication1/Page1.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace WisejWebPageApplication1
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
